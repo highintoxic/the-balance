7841c065e94cda44dae6e2d6d63023a079ce8520
Kuruv Patel <kuruvpatel2005@gmail.com>
2024-07-07 00:57:33 +0530

    Removed Garbage Code
    - Removed garbage code from cards component

--- frontend/src/components/card.jsx
+++ frontend/src/components/card.jsx
@@ -25,10 +25,6 @@ class Card extends Component {
 						</div>
 					</div>
 				</div>
-				<span className='ease absolute left-0 top-0 h-0 w-0 border-t-2 border-violet-600 transition-all duration-200 group-hover:w-full'></span>
-				<span className='ease absolute right-0 top-0 h-0 w-0 border-r-2 border-violet-600 transition-all duration-200 group-hover:h-full'></span>
-				<span className='ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-violet-600 transition-all duration-200 group-hover:w-full'></span>
-				<span className='ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-violet-600 transition-all duration-200 group-hover:h-full'></span>
 			</a>
 		);
 	}
