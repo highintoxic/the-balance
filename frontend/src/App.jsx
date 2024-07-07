cef7c2eaeb36f8d54db988ac5b366365aefde7e5
Kuruv Patel <kuruvpatel2005@gmail.com>
2024-07-07 15:57:38 +0530

    Added NavBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,11 @@
-import Card from "./components/card";
-import links from "./pages.json";
+import Home from './pages/home'
+import NavBar from './components/navbar';
 function App() {
 
 	return (
 		<div>
-			<div className='flex justify-center items-center h-screen space-x-4 bg-primary'>
-				{links.map((x, i) => {
-					return <Card key={i} title={x.title} desc={x.desc} link={x.link} />;
-				})}
-			</div>
+			<NavBar/>
+			<Home/>
 		</div>
 	);
 }
