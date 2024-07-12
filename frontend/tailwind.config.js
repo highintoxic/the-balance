33979fc3c2e9da41b27ecdfacb68c2be9b2c6bcb
Kuruv Patel <kuruvpatel2005@gmail.com>
2024-07-13 03:24:52 +0530

    Added Currency Converted Page
    # Add - Currency Converter
    # Add Dropdown Menu component
    # Fix - tailwind css config to support custom colors again
    # Add - Axios package to make api calls

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,16 +3,15 @@ import scrollbar from "tailwind-scrollbar";
 export default {
 	content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
 	theme: {
-		colors: {
-			primary: "#FFFFF0",
-			secondary: "#FFF8DC",
-			txt: "#000000",
-			pBg: "#3DC2EC",
-			blue: "#587ef4",
+		extend: {
+			colors: {
+				primary: "#FFFFF0",
+				secondary: "#FFF8DC",
+				txt: "#000000",
+				pBg: "#3DC2EC",
+				blue: "#587ef4",
+			}
 		},
-		extend: {},
 	},
-	plugins: [
-		scrollbar
-	],
+	plugins: [scrollbar],
 };
