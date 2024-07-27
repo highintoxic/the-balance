0268d7d34f74ac2bf75e0043b913e2d3d97eabae
Kuruv Patel <kuruvpatel2005@gmail.com>
2024-07-27 15:37:05 +0530

    feat(TutorialCard)

---
+++ frontend/src/components/tutorialCard.jsx
@@ -0,0 +1,65 @@
+import { Component } from "react";
+import PropTypes from "prop-types";
+import YTAlt from "../assets/yt-alt-thumb.png"
+
+export default class TCard extends Component {
+	static propTypes = {
+		channelName: PropTypes.string,
+		videoTitle: PropTypes.string,
+		videoDesc: PropTypes.string,
+		videoLink: PropTypes.string,
+		videoThumb: PropTypes.string,
+	};
+
+	render() {
+		return (
+			<>
+				<div className='flex min-h-screen items-center justify-center'>
+					<div className='relative flex w-full max-w-[48rem] flex-row rounded-xl bg-secondary bg-clip-border text-gray-700 shadow-md'>
+						<div className='relative m-0 w-2/5 shrink-0 overflow-hidden rounded-xl rounded-r-none bg-white bg-clip-border text-gray-700'>
+							<img
+								src={this.props.videoThumb || YTAlt}
+								alt='image'
+								className='h-full w-full object-cover bg-transparent'
+							/>
+						</div>
+						<div className='p-6'>
+							<h6 className='mb-4 block font-sans text-base font-semibold uppercase leading-relaxed tracking-normal text-pink-500 antialiased'>
+								{this.props.channelName}
+							</h6>
+							<h4 className='mb-2 block font-sans text-2xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased'>
+								{this.props.videoTitle}
+							</h4>
+							<p className='mb-8 block font-sans text-base font-normal leading-relaxed text-gray-700 antialiased'>
+								{this.props.videoDesc}
+							</p>
+							<a className='inline-block' href={this.props.videoLink}>
+								<button
+									className='flex select-none items-center gap-2 rounded-lg py-3 px-6 text-center align-middle font-sans text-xs font-bold uppercase text-pink-500 transition-all hover:bg-pink-500/10 active:bg-pink-500/30 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none'
+									type='button'
+								>
+									Watch Now
+									<svg
+										xmlns='http://www.w3.org/2000/svg'
+										fill='none'
+										viewBox='0 0 24 24'
+										strokeWidth='2'
+										stroke='currentColor'
+										aria-hidden='true'
+										className='h-4 w-4'
+									>
+										<path
+											strokeLinecap='round'
+											strokeLinejoin='round'
+											d='M17.25 8.25L21 12m0 0l-3.75 3.75M21 12H3'
+										></path>
+									</svg>
+								</button>
+							</a>
+						</div>
+					</div>
+				</div>
+			</>
+		);
+	}
+}
