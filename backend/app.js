84ed73e87bead845399e31b80adcb11f43695f0a
Kuruv Patel <kuruvpatel2005@gmail.com>
2024-07-28 23:56:46 +0530

    refactor(backend): home route

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+    res.send("<h1>Hello World!</h1>");
 });
 
 app.use(ErrorHandler)
